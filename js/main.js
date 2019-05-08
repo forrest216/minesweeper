72da60bf3c395eae85e1602e857f569e726a868a
Forrest Smith <forrestsmith@Forrests-MacBook-Pro.local>
2019-05-08 16:57:00 -0700

    clean up code, remove comments

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ var state = {
       cust: { r: 10, c: 10, m: 8 },
    },
 };
-/*-------Global Variables---------*/
+
 var win = null;
 var size = 'inter';
 var set = state.diff[size];
@@ -31,10 +31,10 @@ let m = state.mines;
 let p = state.play;
 var leftMouseDown = false;
 var rightMouseDown = false;
-var target = null; // id of most recently clicked tile
-var currentTile = null; // Hidden value of most recently clicked tile
+var target = null;
+var currentTile = null;
 var inPlay = false;
-/*---------References---------*/
+
 const boardArea = document.getElementById('boardArea');
 const board = document.getElementById('board');
 const body = document.getElementById('body');
@@ -44,7 +44,6 @@ const help = document.getElementById('lol');
 const tictoc = document.getElementById('tic');
 const count = document.getElementById('mineCount');
 
-/*---------LISTENERS---------*/
 body.addEventListener('mousedown', (evt) => { if (evt.which == 1) { leftMouseDown = true }; if (evt.which == 3) { rightMouseDown = true } });
 body.addEventListener('mouseup', (evt) => { if (evt.which == 1) { leftMouseDown = false }; if (evt.which == 3) { rightMouseDown = false } });
 gameMenu.addEventListener('click', diffSelect);
@@ -58,9 +57,6 @@ board.addEventListener('mouseup', tileChoose);
 board.addEventListener('contextmenu', tileFlag);
 window.oncontextmenu = () => { return false };
 
-
-
-/*---------HANDLERS---------*/
 function diffMenu() {
    gameMenu.classList.toggle("show");
    help.classList.remove("show");
@@ -92,7 +88,7 @@ function timer() {
    } else { return }
 }
 
-function tilePress(evt) {// Mouse press(but not release) within a null tile shows pressed tile image and smiley braces
+function tilePress(evt) {
    var id = evt.target.id;
    if (state.play[id] == null) {
       evt.target.style.backgroundImage = state[0];
@@ -100,7 +96,7 @@ function tilePress(evt) {// Mouse press(but not release) within a null tile show
    }
 }
 
-function tileEnter(evt) {// If mouse is down and square is null, show a '0' image while leftMouseDown, else leave the image how it is.
+function tileEnter(evt) {
    var id = evt.target.id;
    var bg = evt.target.style.backgroundImage;
    if (leftMouseDown == true && bg == state[null] && state.play[id] == null) {
@@ -108,14 +104,14 @@ function tileEnter(evt) {// If mouse is down and square is null, show a '0' imag
    }
 }
 
-function tileExit(evt) {// If mouse is down when it exits a covered square that was previously covered, the bg image is reset to 'null'
+function tileExit(evt) {
    var id = evt.target.id;
    if (leftMouseDown == true && state.play[id] == null) {
       evt.target.style.backgroundImage = state[null];
    }
 }
 
-function tileChoose(evt) {// If mouse button is released while on a safe square, the contents are uncovered, (board resets if first guess contains mine)
+function tileChoose(evt) {
    var id = evt.target.id;
    if (leftMouseDown && rightMouseDown) console.log('both');
    if (win) return;
@@ -144,11 +140,10 @@ function tileFlag(evt) {
    render();
 }
 
-/*---------FUNCTIONS---------*/
 init();
 setInterval(timer, 1000);
 
-function init() {// Create board arrays and divs, calls loadMines()
+function init() {
    board.style.gridTemplateRows = `repeat(${set.r}, 25px)`;
    board.style.gridTemplateColumns = `repeat(${set.c}, 25px)`;
    for (let i = 0; i < set.r * set.c; i++) {
@@ -165,15 +160,15 @@ function init() {// Create board arrays and divs, calls loadMines()
    loadMines();
 }
 
-function loadMines() {// Randomize 'X's into mines array and populate surrounding square values by calling loadNums()
+function loadMines() {
    while (m.filter(function (y) { return y === 'X' }).length < set.m) {
       m[Math.floor(Math.random() * set.r * set.c)] = 'X';
    };
-   m.forEach(neighbors); // Calls neighbors function on each square, loading surrounding mines count into mines array
+   m.forEach(neighbors);
 }
 
 function avoid() {
-   if (m[target] === 'X' && inPlay === false) { // If mouseup target within board has m[target] === 'X', board is reshuffled, else tile is exposed
+   if (m[target] === 'X' && inPlay === false) {
       while (m[target] === 'X' && inPlay === false) {
          while (board.hasChildNodes()) { board.removeChild(board.childNodes[0]) };
          m.length = 0;
@@ -192,42 +187,42 @@ function avoid() {
 
 function neighbors(tileVal, idx) {
    let top = idx < set.c ? true : false;
-   let right = (idx % set.c + 1) % set.c === 0 ? true : false; // These TRBL check for borders so they can be excluded from results
+   let right = (idx % set.c + 1) % set.c === 0 ? true : false;
    let bottom = idx > m.length - set.c - 1 ? true : false;
    let left = idx % set.c === 0 ? true : false;
-   let radius = [ // An array of objects representing neighboring squares. {val: value contained at id: index of neighboring square}
-      (top || left) ? { val: null } : { val: m[parseInt(idx) - set.c - 1], id: parseInt(idx) - set.c - 1 }, // Above L
-      top ? { val: null } : { val: m[parseInt(idx) - set.c], id: parseInt(idx) - set.c }, // Above
-      (top || right) ? { val: null } : { val: m[parseInt(idx) - set.c + 1], id: parseInt(idx) - set.c + 1 }, // Above R
-      left ? { val: null } : { val: m[parseInt(idx) - 1], id: parseInt(idx) - 1 }, // L
-      right ? { val: null } : { val: m[parseInt(idx) + 1], id: parseInt(idx) + 1 }, // R      Stupid type coercion
-      (bottom || left) ? { val: null } : { val: m[parseInt(idx) + set.c - 1], id: parseInt(idx) + set.c - 1 }, // Below L
-      bottom ? { val: null } : { val: m[parseInt(idx) + set.c], id: parseInt(idx) + set.c }, // Below
-      (bottom || right) ? { val: null } : { val: m[parseInt(idx) + set.c + 1], id: parseInt(idx) + set.c + 1 }, // Below R
+   let radius = [
+      (top || left) ? { val: null } : { val: m[parseInt(idx) - set.c - 1], id: parseInt(idx) - set.c - 1 },
+      top ? { val: null } : { val: m[parseInt(idx) - set.c], id: parseInt(idx) - set.c },
+      (top || right) ? { val: null } : { val: m[parseInt(idx) - set.c + 1], id: parseInt(idx) - set.c + 1 },
+      left ? { val: null } : { val: m[parseInt(idx) - 1], id: parseInt(idx) - 1 },
+      right ? { val: null } : { val: m[parseInt(idx) + 1], id: parseInt(idx) + 1 },
+      (bottom || left) ? { val: null } : { val: m[parseInt(idx) + set.c - 1], id: parseInt(idx) + set.c - 1 },
+      bottom ? { val: null } : { val: m[parseInt(idx) + set.c], id: parseInt(idx) + set.c },
+      (bottom || right) ? { val: null } : { val: m[parseInt(idx) + set.c + 1], id: parseInt(idx) + set.c + 1 },
    ];
-   let acc = radius.reduce((a, val) => { if (val.val == 'X') a++; return a }, 0); // Adds up 'X's in neighboring squares -- this is the # entered into each non-mine square
-   if (tileVal != 'X') { m[idx] = acc; };// Loads #s into mines array if there is no 'X' there
+   let acc = radius.reduce((a, val) => { if (val.val == 'X') a++; return a }, 0);
+   if (tileVal != 'X') { m[idx] = acc; };
    radius.forEach((obj) => {
       if (tileVal === 0 && p[obj.id] === null) {
          p[obj.id] = obj.val;
-         return neighbors(obj.val, obj.id); // RECURSION BREH
+         return neighbors(obj.val, obj.id);
       }
    });
-   return radius; // Array of 8 objects [{ val: , id: },...] (surrounding squares)
+   return radius;
 }
 
 function footPrint(val, idx) {
    var radObjects = neighbors(val, idx);
    if (typeof p[idx] == 'number' && leftMouseDown && rightMouseDown) {
-      let f = 0; // Current footprint flag counter
-      let xs = 0; // Current footprint mine counter
+      let f = 0;
+      let xs = 0;
       let fArray = [];
-      radObjects.forEach((obj) => { // Counts surrounding 'F's and 'X's for comparison
+      radObjects.forEach((obj) => {
          if (p[obj.id] == 'F')++f;
          if (obj.val == 'X')++xs;
          fArray.push(p[obj.id]);
       });
-      radObjects.forEach((obj) => { // Exposes 8-square neighbor 'footprint' when # of flags matches # value of square image
+      radObjects.forEach((obj) => {
          if (f == xs && p[obj.id] !== 'F' && fArray.includes('F')) {
             p[obj.id] = obj.val;
          };
@@ -236,7 +231,7 @@ function footPrint(val, idx) {
    };
 }
 
-function render() { // ----> to be used during prop() and for WIN or LOSS condition (reveal all mines, smiley does ___)
+function render() {
    p.forEach((tileState, idx) => {
       document.getElementById(`${idx}`).style.backgroundImage = state[tileState];
    });
@@ -274,7 +269,7 @@ function reset() {
    smiley.style.backgroundImage = 'url(images/smiley.png)';
 }
 
-function winner() { // Cheating
+function winner() {
    m.forEach((val, idx) => { if (val !== 'X') { p[idx] = val } });
    render();
 }
